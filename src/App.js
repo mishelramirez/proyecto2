0af6c91f73966131bf7de52fe782b5424839f358
cintya <rmishel564@gmail.com>
2023-03-28 19:42:35 -0500

    se añaden estilos en un 70%

--- src/App.js
+++ src/App.js
@@ -9,12 +9,37 @@ function App() {
         <h1 className="title2"> Powered by technology</h1>
         <p className="text"> our artificial intelligence powered tools use millons of project data <br /> points  to ensure thet your project successful</p>
       </div>
-      <div>
-        <Card
-          titulo="Supervisor"
-          texto="Monitors activity to identify project road blocks"
-          src="src/assets/icon-supervisor.svg"
-        />
+      <div className="container">
+        <div className="card1">
+          <Card
+            titulo="Supervisor"
+            texto="Monitors activity to identify project <br/> road blocks"
+            img="imagen1"
+          />
+        </div>
+        <div className="contenedor2">
+          <div className="card2">
+            <Card
+              titulo="Team Builder"
+              texto="Scans our talent network to create the optimal team for your project"
+              src="src/assets/icon-supervisor.svg" />
+          
+          </div>
+
+          <div className="card3">
+            <Card
+              titulo="Karma"
+              texto=" Regularly evaluates our telent to ensure quality"
+              src="src/assets/icon-supervisor.svg" />
+          </div>
+          </div>
+        <div className="card4">
+          <Card
+            titulo="Calculator"
+            texto="Uses data from past projects to provide better delivery estimates"
+            src="assets/http://www.w3.org/2000/svg"
+          />
+        </div>
       </div>
     </div>
   </>;
--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "../Style/cards.css"
 
 
-export const Card = ({titulo, texto, src}) => {
+export const Card = ({titulo, texto, img}) => {
 
     return (
         <div className="contenedor">
             <p className="titulo"><strong> {titulo} </strong></p>
             <p className="texto"> {texto} </p>
-            <img src={src}/>
+           <div className={img} />
         </div>
     );
 }
